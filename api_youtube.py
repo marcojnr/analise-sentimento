da2148b1a2f0f7467ed3026a45fff66704d566f1
marco <marcojunior98@hotmail.com>
2024-09-21 16:06:02 -0300

    ajuste api e add gitignore

--- api_youtube.py
+++ api_youtube.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import re
+import pandas as pd
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
@@ -37,7 +40,6 @@ def get_comment_replies(youtube, parent_id):
             maxResults=100,
             textFormat="plainText"
         ).execute()
-        
         # response = request(youtube, 'comments', parent_id)
         # response.execute()
         
@@ -76,9 +78,13 @@ def save_video_comments(youtube, video_id, filename):
                     replies = get_comment_replies(youtube, parent_id = comment["id"])
                     comments.extend(replies)
             
-            with open(os.path.join(DATA_DIR, filename), "a", encoding="utf-8") as savefile:
-                savefile.write("\n".join(comments))
-                
+            # with open(os.path.join(DATA_DIR, filename), "a", encoding="utf-8") as savefile:
+            #     savefile.write("\n".join(comments))
+            # criar csv
+            df = pd.DataFrame(comments, columns=['Comentarios']).reset_index()
+            print(f"Total de comentários: {len(df)}\n")
+            df.to_csv(filename, encoding="UTF-8", sep=';', index=False)
+            
             if "nextPageToken" in response:
                 token = response["nextPageToken"]
                 response = youtube.commentThreads().list(
@@ -95,28 +101,45 @@ def save_video_comments(youtube, video_id, filename):
         print(e)
 
 
-def count_hashtag(comments, hashtag):
-    counter = 0
-    for comment in comments:
-        if hashtag in comment.lower():
-            counter += 1
-    return counter
+# def count_hashtag(comments, hashtag):
+#     counter = 0
+#     for comment in comments:
+#         if hashtag in comment.lower():
+#             counter += 1
+#     return counter
+
+def id_video_format(lind_video: str):
+    # link = lind_video
+    id_v = re.findall('v=.+$', lind_video)
+    id_v = str(id_v[0])
+    id_v = id_v[2:]
+    if id_v[-1] == "'":
+        id_v = id_v.replace(id_v[-1],"")
+        # print(id_v)
+    return id_v
+
 
 if __name__ == '__main__':
     with open("key-api.txt") as apifile:
         api_key = apifile.read().strip()
     api_name = "youtube"
     api_version = "v3"
-    video_id = "JfJJIrOhWwQ" 
-    filename = "guanabara_IA.txt"
-    hashtag = "#sql"
+    try:
+        input_link = sys.argv[1]
+        video_id = id_video_format(input_link)
+        # video_id = "JfJJIrOhWwQ" 
+        filename = "result.csv"
+        # hashtag = "#sql"
 
-    youtube = build(api_name, api_version, developerKey=api_key)
+        youtube = build(api_name, api_version, developerKey=api_key)
 
-    save_video_comments(youtube, video_id, filename)
+        save_video_comments(youtube, video_id, filename)
 
-    with open(os.path.join(DATA_DIR, filename), encoding="utf-8") as commentsfile:
-        comments = commentsfile.readlines()
-    
-    found = count_hashtag(comments, hashtag)
-    print(f"Total de comentários: {len(comments)}\nCom {hashtag}: {found}")
+        # with open(os.path.join(DATA_DIR, filename), encoding="utf-8") as commentsfile:
+        #     comments = commentsfile.readlines()
+        
+        # found = count_hashtag(comments, hashtag)
+        # print(f"Total de comentários: {len(comments)}\nCom {hashtag}: {found}")
+        # print(f"Total de comentários: {len(comments)}\n")
+    except IndexError as a:
+        print('\nRode novamente! Passando o link do video como paramentro.\n ex:\n api_youtube.py lind_do_video_youtube')

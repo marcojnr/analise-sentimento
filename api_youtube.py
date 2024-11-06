41eda2c8e391dbde72150b3c2987980a449578d5
marco <marcojunior98@hotmail.com>
2024-11-05 23:23:50 -0300

    api: update pegar todos os comentarios

--- api_youtube.py
+++ api_youtube.py
@@ -7,31 +7,8 @@ from googleapiclient.errors import HttpError
 
 DATA_DIR = os.getcwd()+'/'
 
-
-# def request(youtube, tipo_resquest, id_resquest, **kwargs):
-#     # token = kwargs
-#     token = kwargs.get('token')
-#     if tipo_resquest == 'comments':  
-#         response = youtube.comments().list(
-#             part="snippet",
-#             parentId=id_resquest,
-#             maxResults=100,
-#             textFormat="plainText",
-#             pageToken=token
-#         )
-#     else:
-#         response = youtube.commentThreads().list(
-#         part="snippet",
-#         videoId=id_resquest,
-#         maxResults=100,
-#         textFormat="plainText",
-#         pageToken=token
-#     )
-#     return response
-
 def get_comment_replies(youtube, parent_id):
     "Retorna lista de respostas a um comentario"
-
     replies = []
     try:
         response = youtube.comments().list(
@@ -40,9 +17,7 @@ def get_comment_replies(youtube, parent_id):
             maxResults=100,
             textFormat="plainText"
         ).execute()
-        # response = request(youtube, 'comments', parent_id)
-        # response.execute()
-        
+
         for element in response["items"]:
             replies.append(element["snippet"]["textDisplay"])
 
@@ -63,27 +38,26 @@ def save_video_comments(youtube, video_id, filename):
         response = request.execute()
         has_next = True
         page_idx = 0
+        comments = []
         while has_next:
+            qtde = [] # limpar só para a contagem
             page_idx += 1
             print(f"Page: {page_idx}")
-            comments = []
+            
             for item in response["items"]:
                 snippet = item["snippet"]
                 comment = snippet["topLevelComment"]
                 text = comment["snippet"]["textDisplay"]
                 comments.append(text)
+                qtde.append(text)
                 # Pegar respostas tambem!
                 if snippet["totalReplyCount"] > 0:
                     # print(text), se quiser ter um feedback
                     replies = get_comment_replies(youtube, parent_id = comment["id"])
                     comments.extend(replies)
-            
-            # with open(os.path.join(DATA_DIR, filename), "a", encoding="utf-8") as savefile:
-            #     savefile.write("\n".join(comments))
-            # criar csv
-            df = pd.DataFrame(comments, columns=['Comentarios']).reset_index()
-            print(f"Total de comentários: {len(df)}\n")
-            df.to_csv(filename, encoding="UTF-8", sep=';', index=False)
+                    qtde.extend(replies)
+
+            print(f"Comentários: {len(qtde)}\n")
             
             if "nextPageToken" in response:
                 token = response["nextPageToken"]
@@ -96,7 +70,14 @@ def save_video_comments(youtube, video_id, filename):
                 ).execute()
             else:
                 has_next = False
-
+        
+            # criar csv
+            df = pd.DataFrame(comments, columns=['Comentarios']).reset_index()
+            df['Comentarios'] = df['Comentarios'].str.replace('\n', ' ', regex=False).str.replace('\r', '', regex=False)
+            # print(f"Total de comentários: {len(df)}\n")
+            df.to_csv(filename, encoding="UTF-8", sep=';', index=False)
+        print(f"Total de Comentários: {len(comments)}\n")
+            
     except HttpError as e:
         print(e)
 
@@ -134,12 +115,7 @@ if __name__ == '__main__':
         youtube = build(api_name, api_version, developerKey=api_key)
 
         save_video_comments(youtube, video_id, filename)
+        print(f"veja resultado no arquivo -> {filename}")
 
-        # with open(os.path.join(DATA_DIR, filename), encoding="utf-8") as commentsfile:
-        #     comments = commentsfile.readlines()
-        
-        # found = count_hashtag(comments, hashtag)
-        # print(f"Total de comentários: {len(comments)}\nCom {hashtag}: {found}")
-        # print(f"Total de comentários: {len(comments)}\n")
     except IndexError as a:
         print('\nRode novamente! Passando o link do video como paramentro.\n ex:\n api_youtube.py lind_do_video_youtube')
